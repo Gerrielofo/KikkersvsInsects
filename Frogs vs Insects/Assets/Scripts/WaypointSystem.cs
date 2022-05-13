e6f6617ed396714f9e7e74dad11a26bf0617a3c2
Gerrielofo <gerlof.reinink@gmail.com>
2022-05-13 11:33:40 +0200

    Waypoint System

---
+++ Frogs vs Insects/Assets/Scripts/WaypointSystem.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointSystem : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float moveSpeed = 2f;
+    public int waypointIndex = 0;
+
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+    public void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+
+        if(transform.position == waypoints[waypointIndex].transform.position)
+        {
+            waypointIndex += 1;
+        }
+        if(waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+}
