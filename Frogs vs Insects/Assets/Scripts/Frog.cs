030e67f439cf273021e8261f033373fb0322a420
Gerrielofo <gerlof.reinink@gmail.com>
2022-05-17 15:35:30 +0200

    Bullet + impact particles

--- Frogs vs Insects/Assets/Scripts/Frog.cs
+++ Frogs vs Insects/Assets/Scripts/Frog.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class Frog : MonoBehaviour
 {
     private Transform target;
+
+    [Header("Attributes")]
+    
     public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]
 
     public Transform partToRotate;
     public float turnSpeed = 10f;
 
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
     public string enemyTag = "Enemy";
+
+
     
     void Start()
     {
@@ -51,6 +63,24 @@ public class Frog : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+        fireCountdown -= Time.deltaTime;
+
+    }
+    void Shoot()
+    {
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
     }
     void OnDrawGizmosSelected()
     {
