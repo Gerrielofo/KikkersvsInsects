954292dfc1ba0dc775d5b33d8ef36343bc0dcdda
Gerrielofo <gerlof.reinink@gmail.com>
2022-05-19 15:15:37 +0200

    ShopBegin

---
+++ Frogs vs Insects/Assets/Scripts/Shop.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public void KoopBert()
+    {
+        Debug.Log("Bert gekocht POGGERS");
+    }
+    public void KoopEdward()
+    {
+        Debug.Log("Edward gekocht POGGERS");
+    }
+    public void KoopEsmee()
+    {
+        Debug.Log("Esmee gekocht POGGERS");
+    }
+    public void KoopBob()
+    {
+        Debug.Log("Bob gekocht POGGERS");
+    }
+    public void KoopPaul()
+    {
+        Debug.Log("Paul gekocht POGGERS");
+    }
+}
