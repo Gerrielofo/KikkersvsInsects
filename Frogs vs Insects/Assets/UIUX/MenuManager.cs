f3647c803fc76ea14c9097eeb245ee8c9d7a0229
Gerrielofo <gerlof.reinink@gmail.com>
2022-06-01 14:54:44 +0200

    Volume slider Menu

---
+++ Frogs vs Insects/Assets/UIUX/MenuManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject optionsUI;
+    public bool wantOptions = false;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && wantOptions == true)
+        {
+            OptionsMenuOff();
+        }
+    }
+    public void OptionsMenuOn()
+    {
+        optionsUI.SetActive(true);
+        wantOptions = true;
+    }
+
+    public void OptionsMenuOff()
+    {
+        optionsUI.SetActive(false);
+        wantOptions = false;
+    }
+}
---
+++ Frogs vs Insects/Assets/UIUX/SoundManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        }
+
+        else
+        {
+            load();
+        }
+
+
+    }
+    public void ChangeVolume(float newVolume)
+    {
+        //AudioListener.volume = newVolume;
+        AudioListener.volume = volumeSlider.value;
+        save();
+    }
+
+    private void load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+
+}
