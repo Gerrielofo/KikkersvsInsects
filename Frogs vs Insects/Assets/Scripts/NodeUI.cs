da72b927b9872e143a46296f62638d73e1e30a86
yolojoramking <joram.klinkenberg@gmail.com>
2022-06-10 11:43:48 +0200

    safety push

---
+++ Frogs vs Insects/Assets/Scripts/NodeUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeUI : MonoBehaviour
+{
+    private Nodes target;
+
+    public void SetTarget (Nodes _target)
+    {
+        target = _target;
+    }
+}
