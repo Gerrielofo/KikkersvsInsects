4cf5df4d296435719b6c6983fa94c35c6f8916d4
Gerrielofo <gerlof.reinink@gmail.com>
2022-06-17 14:03:24 +0200

    MainMenu UI Resolution

--- Frogs vs Insects/Assets/UIUX/MenuManager.cs
+++ Frogs vs Insects/Assets/UIUX/MenuManager.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject optionsUI;
     public static bool wantOptions = false;
+    public GameObject buttonsUI;
 
 
     private void Update()
@@ -17,16 +19,33 @@ public class MenuManager : MonoBehaviour
     {
         optionsUI.SetActive(true);
         wantOptions = true;
+        buttonsUI.SetActive(false);
     }
 
     public void OptionsMenuOff()
     {
         optionsUI.SetActive(false);
         wantOptions = false;
+        buttonsUI.SetActive(true);
     }
 
     public void ExitGame()
     {
         Application.Quit();
+    }
+    List<int> widths = new List<int>() { 568, 960, 1280, 1920 };
+    List<int> heights = new List<int>() { 320, 540, 800, 1080 };
+
+    public void SetScreenSize(int index)
+    {
+        bool fullscreen = Screen.fullScreen;
+        int width = widths[index];
+        int height = heights[index];
+        Screen.SetResolution(width, height, fullscreen);
+    }
+
+    public void SetFullscreen(bool _fullscreen)
+    {
+        Screen.fullScreen = _fullscreen;
     }
 }
