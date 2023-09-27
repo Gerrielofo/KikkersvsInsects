90e4175a2867d988fc732185305c1db61009c524
MelKaraa <boyka11@hotmail.nl>
2023-09-27 16:49:57 +0200

    prop

--- Frogs vs Insects/Assets/Scripts/DevTools.cs
+++ Frogs vs Insects/Assets/Scripts/DevTools.cs
@@ -9,7 +9,7 @@ public class DevTools : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
-            PlayerStats.Money += 100;
+            PlayerStats.Money += 100000;
         }
 
         if (Input.GetKeyDown(KeyCode.L))
